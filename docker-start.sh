b2c89165a26a811668ae008c08bdd37bbb0961cc
chrispaganon <info@chrispaganon.com>
2023-10-27 16:18:27 -0400

    Segregate all the networks

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,8 @@
-docker network create vue-ai_caddy_network
+docker network create vue-ai_production_app_caddy
+docker network create vue-ai_staging_app_caddy
+docker network create vue-ai_production_db_caddy
+docker network create vue-ai_staging_db_caddy
+docker network create vue-ai_umami_caddy
+
 docker compose up --force-recreate -d
 docker compose -f vue-ai/vue-ai-compose.yml up --force-recreate -d
