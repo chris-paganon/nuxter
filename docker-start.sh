44bf0aa58d271a11bc2dc3f6457ae0d88c3d5a89
chrispaganon <info@chrispaganon.com>
2023-10-31 14:48:55 -0400

    Use consistent app name

--- docker-start.sh
+++ docker-start.sh
@@ -1,11 +1,11 @@
-docker network create vue-ai_production_app_caddy
-docker network create vue-ai_staging_app_caddy
-docker network create vue-ai_production_db_caddy
-docker network create vue-ai_staging_db_caddy
-docker network create vue-ai_umami_caddy
+docker network create vue-tools-ai_production_app_caddy
+docker network create vue-tools-ai_staging_app_caddy
+docker network create vue-tools-ai_production_db_caddy
+docker network create vue-tools-ai_staging_db_caddy
+docker network create vue-tools-ai_umami_caddy
 
 docker compose up --force-recreate -d
-docker compose -f vue-ai/vue-ai-compose.yml up --build --remove-orphans
+docker compose -f vue-tools-ai/vue-tools-ai-compose.yml up --build --remove-orphans
 
 # TODO: Add a new file for deployments
 # To deploy: use docker compose --p "project-name-blue" up & docker compose --p "project-name-green" up (or --project-name) to make a blue-green deployment. Maybe a similar simple solution here: https://www.tines.com/blog/simple-zero-downtime-deploys-with-nginx-and-docker-compose
