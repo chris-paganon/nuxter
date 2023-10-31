bb20fa70167735631217b049b88cbc860d54a99e
chrispaganon <info@chrispaganon.com>
2023-10-31 01:35:09 -0400

    Fix git cache invalidation

--- docker-start.sh
+++ docker-start.sh
@@ -5,4 +5,10 @@ docker network create vue-ai_staging_db_caddy
 docker network create vue-ai_umami_caddy
 
 docker compose up --force-recreate -d
-docker compose -f vue-ai/vue-ai-compose.yml up --force-recreate -d
+docker compose -f vue-ai/vue-ai-compose.yml up --build
+
+# TODO: Add a new file for deployments
+# To deploy: use docker compose --p "project-name-blue" up & docker compose --p "project-name-green" up (or --project-name) to make a blue-green deployment. Maybe a similar simple solution here: https://www.tines.com/blog/simple-zero-downtime-deploys-with-nginx-and-docker-compose
+# Still need to figure out how Caddy interacts with that though.
+
+# Alternatively: use docker swarm or Traefik apparently has features that may integrate better with docker (aka labels)
