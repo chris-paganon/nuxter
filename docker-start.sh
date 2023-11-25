248ec043b3922971ed0a05cfcad5e6c708711bd5
chrispaganon <info@chrispaganon.com>
2023-11-25 14:49:07 -0500

    Enable seperate prod & staging .env

--- docker-start.sh
+++ docker-start.sh
@@ -5,7 +5,8 @@ docker network create vue-tools-ai_staging_db_caddy
 docker network create vue-tools-ai_umami_caddy
 
 docker compose up --force-recreate -d
-docker compose -f vue-tools-ai/vue-tools-ai-compose.yml up --build --remove-orphans
+docker compose -f vue-tools-ai/vue-tools-ai-production-compose.yml up --build --remove-orphans -d
+docker compose -f vue-tools-ai/vue-tools-ai-staging-compose.yml up --build --remove-orphans -d
 
 # TODO: Add a new file for deployments
 # To deploy: use docker compose --p "project-name-blue" up & docker compose --p "project-name-green" up (or --project-name) to make a blue-green deployment. Maybe a similar simple solution here: https://www.tines.com/blog/simple-zero-downtime-deploys-with-nginx-and-docker-compose
