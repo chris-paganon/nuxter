5aa45143136e367af3460290e261be90ba47832b
chrispaganon <info@chrispaganon.com>
2023-11-29 23:11:58 -0500

    Add some TODOs

--- docker-start.sh
+++ docker-start.sh
@@ -13,3 +13,19 @@ docker compose -f vue-tools-ai/vue-tools-ai-staging-compose.yml up --build --rem
 # Still need to figure out how Caddy interacts with that though.
 
 # Alternatively: use docker swarm or Traefik apparently has features that may integrate better with docker (aka labels)
+
+# Example command to restart a single container for deployment:
+# docker compose -f vue-tools-ai/vue-tools-ai-staging-compose.yml up --build --no-deps -d nuxt-app
+
+# Example to deploy a new version of the app with zero downtime (untested):
+# docker rename vue-tools-ai-production-nuxt-app vue-tools-ai-production-nuxt-app-green
+# docker compose -f base/nuxt-app-compose.yml -p vue-tools-ai-production-blue --env-file vue-tools-ai/production.env up
+# docker compose -f vue-tools-ai/vue-tools-ai-staging-compose.yml up --build --no-deps -d nuxt-app
+# docker rename vue-tools-ai-production-nuxt-app vue-tools-ai-production-nuxt-app-blue
+# docker rename vue-tools-ai-production-nuxt-app-green vue-tools-ai-production-nuxt-app
+# docker compose down vue-tools-ai-production-blue
+
+# Alternatively, we can run this (untested):
+# docker compose -f base/nuxt-app-compose.yml -p vue-tools-ai-production-blue --env-file vue-tools-ai/production.env up
+# To make it work, this line "container_name: ${APP_NAME}-${APP_ENV}-nuxt-app" in nuxt-app-compose.yml needs a way to set a new name for the temporary container, otherwise it will fail because the container already exists. This is why we rename containers above.
+# Then we would run blue-green deployment similar to above.
