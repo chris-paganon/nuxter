6769aaff85b210e60e7f48d84fea537293d97a11
chrispaganon <info@chrispaganon.com>
2024-01-02 01:12:35 -0500

    Add basic init test command

---
+++ cli/cmd/init.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// timezoneCmd represents the timezone command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		os.Mkdir("test", 0755)
+		// Git clone this repository: https://github.com/chris-paganon/jebatimatech
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// timezoneCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// timezoneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
